5588eb9fc58a05eb98403d8ea96831cafc5f6937
Pedro Trujillo <pedroetb@gmail.com>
2024-03-06 00:42:33 +0000

    Borra restos de variables y limpia health

--- deploy/config/entrypoint.sh
+++ deploy/config/entrypoint.sh
@@ -4,10 +4,8 @@ cat ${CONFIG_PATH}/alertmanager-tmpl.yml \
 	| sed "s@\${SLACK_URL}@${SLACK_URL}@g" \
 	| sed "s@\${SLACK_CHANNEL}@${SLACK_CHANNEL}@g" \
 	| sed "s@\${SLACK_USER}@${SLACK_USER}@g" \
-	| sed "s@\${SLACK_TEXT_TEMPLATE}@${SLACK_TEXT_TEMPLATE}@g" \
 	| sed "s@\${TELEGRAM_CHAT_ID}@${TELEGRAM_CHAT_ID}@g" \
 	| sed "s@\${TELEGRAM_BOT_TOKEN}@${TELEGRAM_BOT_TOKEN}@g" \
-	| sed "s@\${TELEGRAM_MESSAGE_TEMPLATE}@${TELEGRAM_MESSAGE_TEMPLATE}@g" \
 	| sed "s@\${TELEGRAM_PARSE_MODE}@${TELEGRAM_PARSE_MODE}@g" \
 	| sed "s@\${TELEGRAM_DISABLE_NOTIFICATIONS}@${TELEGRAM_DISABLE_NOTIFICATIONS}@g" \
 	> ${CONFIG_PATH}/alertmanager.yml
